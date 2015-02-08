c4750c85399eaf8a87c1e77c5bd9f7f1f22e382c
Estelle Weyl <estelle@weyl.org>
2015-02-08 14:50:49 -0800

    as foreground and as background. Support for Safari

--- static/js/flickr.js
+++ static/js/flickr.js
@@ -3,17 +3,19 @@ var flickr =  {
     // event handler for submit
     document.getElementById('submit').addEventListener('click', function (e) {
       flickr.flickrSearch();
+      e.preventDefault();
       return false;
     });
   },
 
   // AJAX Request
   flickrSearch : function () {
-    var flickrResponse = new XMLHttpRequest(),
-    searchTerm = document.getElementById('search').value,
-    url = "https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&content_type=1&media=photos&api_key=867146aba0e148d0a206b4d671a6045a&tags="+ escape(searchTerm) + "&per_page=60&format=json";
+    var flickrResponse,
+      searchTerm = document.getElementById('search').value,
+      url = "https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&content_type=1&media=photos&api_key=867146aba0e148d0a206b4d671a6045a&tags="+ escape(searchTerm) + "&per_page=60";
 
     if (searchTerm) {
+      flickrResponse = new XMLHttpRequest();
       flickrResponse.open("POST", url, true);
       flickrResponse.onreadystatechange = flickr.processResponse;
       flickrResponse.send();
@@ -24,11 +26,23 @@ var flickr =  {
 
   // AJAX Handler
   processResponse : function (e) {
+    var response;
     if(e.target.readyState === 4 && e.target.status===200) {
         //flickr server response
-        var response = e.target.response;
-        eval(response);
+        response = "flickr." + e.target.response;
+        flickr.attachResponse(response);
     }
+  },
+
+  attachResponse: function (response) {
+    var script = document.getElementById('tempJS');
+    if(script) {
+      script.parentNode.removeChild(script);
+    }
+    script = document.createElement('script');
+    script.setAttribute('id', 'tempJS');
+    script.innerHTML = response;
+    document.body.appendChild(script);
   },
 
   // handle data layout the page
@@ -44,20 +58,27 @@ var flickr =  {
   },
 
   // create one figure
-  createAnImage: function (photo) {
+  // for index.html (version 1)
+ createAnImage: function (photo) {
    return '<figure><img id="' +
       photo.id + '" style="background-image: url(http://farm' +
       photo.farm + '.static.flickr.com/' +
       photo.server + '/' +
       photo.id + '_' +
-      photo.secret + '_n.jpg)" src="static/assets/trans.gif" alt="' +
+      photo.secret + '_n.jpg)" src="static/assets/trans.gif" alt="' + // _n is for small img
       photo.title + '" /><figcaption>' +
       photo.title + '</figcaption></figure>';
-  }
+  }/*,
+   // for version 2 (index2.html)
+  createAnImage: function (photo) {
+   return '<figure><img id="' +
+      photo.id + '" src="http://farm' +
+      photo.farm + '.static.flickr.com/' +
+      photo.server + '/' +
+      photo.id + '_' +
+      photo.secret + '_n.jpg)" alt="' + // _n is for small img
+      photo.title + '" /></figure>';
+  }*/
 };
 
 flickr.init();
-
-function jsonFlickrApi(data) {
-  flickr.jsonFlickrApi(data);
-}
